23f24f6f9a69bde84fc87b626c021c06b2a94820
nihaprezz <sergio13prez@gmail.com>
2019-11-08 12:40:48 -0500

    burger controller almost done

--- app/controllers/burgers_controller.rb
+++ app/controllers/burgers_controller.rb
@@ -1,2 +1,18 @@
 class BurgersController < ApplicationController
+    def index
+        burgers = Burger.all 
+        render json: burgers
+    end
+
+    def show
+        burger = Burger.find(params[:id]);
+        render json: burger
+    end
+
+    def ingredients   
+        recipeID = params['id']
+        render json: thisBurgersIngredients = Burger.where("recipe_id = ? ", recipeID)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :burgers
-  resources :ingredients
-  resources :recipes
+  resources :burgers, only: [:index, :show]
+  resources :ingredients, only: [:index, :show]
+  resources :recipes, only: [:index, :show]
+  
+  get '/burgers_recipe/:id', to: 'burgers#ingredients'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
