201e397bdcbaf579a3cbe23705ecbfad68c49615
github username <github email address>
2019-11-08 12:36:59 -0500

    ingredients_controller finished

--- app/controllers/burgers_controller.rb
+++ app/controllers/burgers_controller.rb
@@ -1,2 +1,3 @@
 class BurgersController < ApplicationController
+
 end
--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,13 @@
 class IngredientsController < ApplicationController
+
+  def index
+    ingredients = Ingredient.all
+    render json: ingredients.to_json(except: [:created_at, :updated_at])
+  end
+
+  def show
+    ingredient = Ingredient.find_by(id: params[:id])
+    render json: ingredient.slice(:id, :name, :image_url)
+  end
+
 end
